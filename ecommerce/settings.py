b32e0552d88037249f1ff54b891b3bed26ba36a0
DhrutiYadav <dhrutiyadav13@gmail.com>
2026-03-11 12:17:48 +0530

    if local network same then runs the website in any device

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'replace-me-in-production'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
