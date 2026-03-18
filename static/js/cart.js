2b364d20a5d09c02661428cdbd6b35237440bed1
DhrutiYadav <dhrutiyadav13@gmail.com>
2026-03-18 22:44:10 +0530

    product_list works correctly

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,104 +1,94 @@
 document.addEventListener('DOMContentLoaded', function () {
-var updateBtns = document.getElementsByClassName('update-cart');
+    var updateBtns = document.getElementsByClassName('update-cart');
 
-for (var i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function () {
+    for (var i = 0; i < updateBtns.length; i++) {
+        updateBtns[i].addEventListener('click', function () {
 
-		var productId = this.dataset.product;
-		var action = this.dataset.action;
+            var productId = this.dataset.product;
+            var action = this.dataset.action;
 
-		// ✅ Try to get size & color from button (cart page)
-		var size = this.dataset.size || "";
-        var color = this.dataset.color || "";
+            // ✅ Try to get size & color from button (cart page)
+            var size = this.dataset.size || "";
+            var color = this.dataset.color || "";
 
-        // 🔒 Fix Django None → JS string issue
-        if (size === "None") size = "";
-        if (color === "None") color = "";
+            // 🔒 Fix Django None → JS string issue
+            if (size === "None") size = "";
+            if (color === "None") color = "";
 
-        if (user === 'AnonymousUser') {
-			alert("Please login to manage cart");
-			return;
-		}
+            if (user === 'AnonymousUser') {
+                alert("Please login to manage cart");
+                return;
+            }
 
-		// ✅ If not found, get from selected inputs (product detail page)
-		if (!size) {
-	        var sizeInput = document.getElementById("selected-size");
-	        if (sizeInput && sizeInput.value) {
-		        size = sizeInput.value;
-	        }
-//          else {
-//		        size = "";
-//	        }
-        }
+            // ✅ If not found, get from selected inputs (product detail page)
+            if (!size) {
+                var sizeInput = document.getElementById("selected-size");
+                if (sizeInput && sizeInput.value) {
+                    size = sizeInput.value;
+                }
+            }
 
+            if (!color) {
+                var colorInput = document.getElementById("selected-color");
+                if (colorInput && colorInput.value) {
+                    color = colorInput.value;
+                }
+            }
 
-		if (!color) {
-	        var colorInput = document.getElementById("selected-color");
-	        if (colorInput && colorInput.value) {
-		        color = colorInput.value;
-	        }
-//	        else {
-//		        color = "";
-//	        }
-        }
+            // 🧹 Remove fake "Free"
+            if (size === "Free") {
+                size = "";
+            }
 
+            // 🚨 VALIDATION
+            var sizeOptions = document.querySelectorAll(".size-option");
+            var colorOptions = document.querySelectorAll(".color-option");
 
-		console.log('productId:', productId, 'Action:', action, 'Size:', size, 'Color:', color);
-		console.log('USER:', user);
-		updateUserOrder(productId, action, size, color);
-	});
-}
+            var hasSizeOptions = sizeOptions.length > 0;
+            var hasColorOptions = colorOptions.length > 0;
 
-/* ---------- LOGGED IN USER ---------- */
+            if (hasSizeOptions && !size) {
+                alert("Please select size");
+                return;
+            }
 
-function updateUserOrder(productId, action, size, color) {
-	console.log('User is logged in, sending data..');
+            if (hasColorOptions && !color) {
+                alert("Please select color");
+                return;
+            }
+            console.log("🛒 FINAL DATA:", {
+                productId,
+                action,
+                size,
+                color
+            });
+            // ✅ Send to backend
+            updateUserOrder(productId, action, size, color);
+        });
+    }
+    /* ---------- LOGGED IN USER ---------- */
+    function updateUserOrder(productId, action, size, color) {
+        console.log('User is logged in, sending data..');
 
-	var url = '/update_item/';
+        var url = '/update_item/';
 
-	fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-CSRFToken': csrftoken,
-		},
-		body: JSON.stringify({
-			'productId': productId,
-			'action': action,
-			'size': size,
-			'color': color
-		})
-	})
-	.then((response) => response.json())
-	.then((data) => {
-		console.log('Data:', data);
-		location.reload();
-	});
-
-}
-
-
-/* ---------- ANONYMOUS USER ---------- */
-//
-//function addCookieItem(productId, action, size, color) {
-//	console.log('User is not logged in');
-//
-//	var key = productId + "_" + size + "_" + color;   // ✅ unique per variant
-//
-//	if(action == 'add'){
-//        updateUserOrder(productId, action, size, color)
-//    }
-//    else if(action == 'remove'){
-//        updateUserOrder(productId, action, size, color)
-//    }
-//    else if(action == 'delete'){
-//        // send remove until item disappears
-//        updateUserOrder(productId, 'remove', size, color)
-//    }
-//
-//
-//
-//	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
-//	location.reload();
-//}
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken,
+            },
+            body: JSON.stringify({
+                'productId': productId,
+                'action': action,
+                'size': size,
+                'color': color
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Data:', data);
+            location.reload();
+        });
+    }
 });
